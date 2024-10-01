2678fa245aebb36ef32cba122a3fad2cfb7279f1
ayeshaa-sn <ayeshaanis@iut-dhaka.edu>
2024-10-01 12:24:15 +0600

    Added a class

---
+++ BookListTracker.java
@@ -0,0 +1,22 @@
+public class BookListTracker {
+
+    public void totalNumOfBooks(){
+
+    }
+
+    public void numOfBooksReadEachMonth(){
+
+    }
+
+    public void Top10Books(){
+
+    }
+
+    public void mostReadGenre(){
+
+    }
+
+    public void averageLength(){
+
+    }
+}
--- BookReaderFactory.java
+++ BookReaderFactory.java
@@ -3,9 +3,11 @@ public class BookReaderFactory {
     public static BookReader getBookReader(String filePath) {
         if (filePath.endsWith(".csv")) {
             return new CsvBookReader();
-        } else if (filePath.endsWith(".json")) {
+        }
+        else if (filePath.endsWith(".json")) {
             return new JsonBookReader();
-        } else if (filePath.endsWith(".xml")) {
+        }
+        else if (filePath.endsWith(".xml")) {
             return new XmlBookReader();
         }
         throw new IllegalArgumentException("Unsupported file format: " + filePath);
